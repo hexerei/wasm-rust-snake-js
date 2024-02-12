7d166408f550e29cc0364b274751def43986ac79
Daniel Vorhauer <daniel.vorhauer@dxc.com>
2024-02-12 19:12:26 +0100

    load wasm

--- www/index.js
+++ www/index.js
@@ -1,4 +1,11 @@
-function init() {
-    alert("Hello World!");
+async function init() {
+
+    const response = await fetch("test.wasm");
+    const buffer = await response.arrayBuffer();
+    const wasm = await WebAssembly.instantiate(buffer);
+
+    const sum = wasm.instance.exports.addTwo;
+    const result = sum(200, 100);
+    console.log(result);
 }
 init();
