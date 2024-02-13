fc51934c4aa1d9f3c013bf39d3a42b3d9a89a3b4
Daniel Vorhauer <daniel.vorhauer@dxc.com>
2024-02-13 15:29:51 +0100

    added canvas

--- www/index.js
+++ www/index.js
@@ -2,5 +2,8 @@ import init, { World } from "snake";
 
 init().then(_ => {
   const world = World.new();
+  const canvas = document.getElementById("snake-canvas");
+  const ctx = canvas.getContext("2d");
+
   console.log(world.width());
 })
