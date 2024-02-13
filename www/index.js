7a335deaed64e7a681b5e03f6108ea4ad465b0f9
Daniel Vorhauer <daniel.vorhauer@dxc.com>
2024-02-13 12:40:40 +0100

    added snake package

--- www/index.js
+++ www/index.js
@@ -1,36 +1,5 @@
-async function init() {
+import init, { greet } from "snake";
 
-    // create wasm memory page
-    const memory = new WebAssembly.Memory({initial: 1})
-
-    // js objects exported to wasm
-    const importObject = {
-        js: {
-            mem: memory
-        },
-        console: {
-          log: () => {
-            console.log("Just logging something!");
-          },
-          error: () => {
-            console.log("I am just error");
-          }
-        }
-    }
- 
-    // instantiate wasm
-    const response = await fetch("test.wasm");
-    const buffer = await response.arrayBuffer();
-    const wasm = await WebAssembly.instantiate(buffer, importObject);
-
-    // test sum function
-    const sum = wasm.instance.exports.addTwo;
-    const result = sum(200, 100);
-    console.log(result);
-
-    // test wasm memory
-    const uint8Array = new Uint8Array(memory.buffer, 0, 2);
-    const hiText = new TextDecoder().decode(uint8Array);
-    console.log(hiText);
-}
-init();
+init().then(_ => {
+  greet("Daniel");
+})
