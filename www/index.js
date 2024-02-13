07ce9259d2177a939be8037ea7ae66ef935d1a8d
Daniel Vorhauer <daniel.vorhauer@dxc.com>
2024-02-13 09:29:49 +0100

    import js to wasm

--- www/index.js
+++ www/index.js
@@ -1,8 +1,19 @@
 async function init() {
 
+    const importObject = {
+        console: {
+          log: () => {
+            console.log("Just logging something!");
+          },
+          error: () => {
+            console.log("I am just error");
+          }
+        }
+    }
+ 
     const response = await fetch("test.wasm");
     const buffer = await response.arrayBuffer();
-    const wasm = await WebAssembly.instantiate(buffer);
+    const wasm = await WebAssembly.instantiate(buffer, importObject);
 
     const sum = wasm.instance.exports.addTwo;
     const result = sum(200, 100);
