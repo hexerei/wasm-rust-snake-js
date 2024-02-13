fc8c23cf866c39c7d8aea20cc1dfeb4c12bb7a10
Daniel Vorhauer <daniel.vorhauer@dxc.com>
2024-02-13 15:54:30 +0100

    bigger cells

--- www/index.js
+++ www/index.js
@@ -1,7 +1,7 @@
 import init, { World } from "snake";
 
 init().then(_ => {
-  const CELL_SIZE = 10;
+  const CELL_SIZE = 20;
   const world = World.new();
   const worldWidth = world.width();
 
@@ -14,11 +14,13 @@ init().then(_ => {
   function drawWorld() {
     ctx.beginPath();
 
+    // vertical grid lines
     for (let x = 0; x < worldWidth + 1; x++) {
       ctx.moveTo(CELL_SIZE * x, 0);
       ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE)
     }
 
+    // horizontal grid lines
     for (let y = 0; y < worldWidth + 1; y++) {
       ctx.moveTo(0, CELL_SIZE * y);
       ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y)
