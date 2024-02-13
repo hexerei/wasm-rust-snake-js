05bc72066fb51c403869b180bd50e19f545e4049
Daniel Vorhauer <daniel.vorhauer@dxc.com>
2024-02-13 13:05:04 +0100

    bootstrap entry point

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,16 +2,16 @@ const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: "./index.js",
+    entry: "./bootstrap.js",
     output: {
         path: path.resolve(__dirname, "public"),
-        filename: "index.js"
+        filename: "bootstrap.js"
     },
     mode: "development",
     plugins: [
         new CopyWebpackPlugin({
             patterns: [
-                { from: "./index.html", to: "./"}
+                { from: "./index.html", to: "./" }
             ]
 
         })
