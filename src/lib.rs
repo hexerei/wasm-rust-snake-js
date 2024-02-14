d9bc01d19bc49d9a9183f47e14515b1d86779269
Daniel Vorhauer <daniel.vorhauer@dxc.com>
2024-02-14 12:04:50 +0100

    snake direction handling

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,28 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left
+}
+
 struct SnakeCell(usize);
 
 struct Snake {
-    body: Vec<SnakeCell>
+    body: Vec<SnakeCell>,
+    direction: Direction
 }
 
 impl Snake {
-    fn new(spawn_index: usize) -> Snake {
+    fn new(spawn_index: usize, dir: Direction) -> Snake {
         Snake {
-            body: vec!(SnakeCell(spawn_index))
+            body: vec!(SnakeCell(spawn_index)),
+            direction: dir
+
         }
     }
-    
 }
 
 #[wasm_bindgen]
@@ -29,24 +38,53 @@ pub struct World {
 
 #[wasm_bindgen]
 impl World {
-    pub fn new(width: usize, snake_idx: usize) -> World {
+    pub fn new(width: usize, snake_idx: usize, snake_dir: u8) -> World {
         World {
             width,
             size: width * width,
-            snake: Snake::new(snake_idx)
+            snake: Snake::new(snake_idx, match snake_dir {
+                0 => Direction::Up,
+                1 => Direction::Right,
+                2 => Direction::Down,
+                _ => Direction::Left
+            })
         }
     }
 
     pub fn update(&mut self) {
         let snake_idx = self.snake_head_idx();
-        self.snake.body[0].0 = (snake_idx + 1) % self.size;
+        let (row, col) = self.index_to_cell(snake_idx);
+        let (row, col) = match self.snake.direction {
+            Direction::Right    => (row, (col + 1) % self.width),
+            Direction::Left     => (row, (col - 1) % self.width),
+            Direction::Up       => ((row - 1) % self.width, col),
+            Direction::Down     => ((row + 1) % self.width, col),
+        };
+
+        self.set_snake_head_idx(self.cell_to_index(row, col));
     }
 
     pub fn width(&self) -> usize {
         self.width
     }
 
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
     pub fn snake_head_idx(&self) -> usize {
         self.snake.body[0].0
+    }
+
+    fn set_snake_head_idx(&mut self, idx: usize) {
+        self.snake.body[0].0 = idx;
+    }
+
+    fn cell_to_index(&self, row: usize, col: usize) -> usize {
+        (row * self.width) + col
+    }
+
+    fn index_to_cell(&self, idx: usize) -> (usize, usize) {
+        (idx / self.width, idx % self.width)
     }
 }
--- www/index.ts
+++ www/index.ts
@@ -3,9 +3,10 @@ import init, { World } from "snake";
 init().then(_ => {
   const CELL_SIZE = 20;
   const WORLD_WIDTH = 8;
+  const SNAKE_DIR = 3;
   const snakeSpawnIdx = Date.now() % (WORLD_WIDTH * WORLD_WIDTH);
 
-  const world = World.new(WORLD_WIDTH, snakeSpawnIdx);
+  const world = World.new(WORLD_WIDTH, snakeSpawnIdx, SNAKE_DIR);
   const worldWidth = world.width();
 
   const canvas = <HTMLCanvasElement> document.getElementById("snake-canvas");
